5d5fbea57e455c9ce3d7680ffa83f4b4c836ea7e
Jacob Allen <133789555+HacobAllon@users.noreply.github.com>
2025-10-11 02:32:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Make all assets relative, fixes white screen on deployment
   plugins: [react()],
-  base: '/VATPHIL',
-  server: {
-    port: 5173,
-    open: true
-  }
+  build: {
+    outDir: 'dist', // Output folder for production build
+    assetsDir: 'assets', // Where static assets (images, CSS, JS) go
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
 });
