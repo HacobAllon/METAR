ab9b2e65841e8c2ed1a59b72c7e105e697acf8fc
Jacob Allen <133789555+HacobAllon@users.noreply.github.com>
2025-10-11 13:18:08 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import MetarRPLL from './MetarRPLL';
+import VATPHILAIO from './VATPHILAIO';
 import './App.css';
+
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root container not found');
 
-createRoot(container).render(<MetarRPLL />);
+createRoot(container).render(<VATPHILAIO />);
