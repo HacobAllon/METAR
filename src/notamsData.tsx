d63df3c868064b0c1756696370862736637d35c6
Jacob Allen <133789555+HacobAllon@users.noreply.github.com>
2025-10-12 09:45:03 +0200

    Add files via upload

---
+++ src/notamsData.tsx
@@ -0,0 +1,23 @@
+export const localNotams = [
+  {
+    id: 1,
+    icao: 'RPLL',
+    message: 'TEST',
+    start: '2025-10-10',
+    end: '2025-10-25',
+  },
+  {
+    id: 2,
+    icao: 'RPLL',
+    message: 'Willie sucks',
+    start: '2025-10-10',
+    end: '2025-10-10',
+  },
+  {
+    id: 2,
+    icao: 'VATPHIL',
+    message: 'Willie does NOT suck',
+    start: '2025-10-10 1000Z',
+    end: '2025-10-10 1000Z',
+  },
+];
