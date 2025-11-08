c9f0f232b3bdbc66c6682371c57d0409eaf68f27
Jacob Allen <133789555+HacobAllon@users.noreply.github.com>
2025-11-08 10:33:19 +0100

    Update NOTAM messages and dates in notamsData.tsx

--- src/notamsData.tsx
+++ src/notamsData.tsx
@@ -6,16 +6,16 @@ export const localNotams = [
     id: 1,
     icao: 'RPLL',
     message:
-      'test',
+      '',
     start: '',
     end: '',
   },
   {
     id: 2,
     icao: 'VATPHIL',
-    message: 'AIRAC2510 is released https://vats.im/ph/2510',
-    start: '2025-10-02 0400Z',
-    end: '2025-10-30 0400Z',
+    message: 'AIRAC2511 is released https://vats.im/ph/2511',
+    start: '2025-11-03 0400Z',
+    end: '2025-11-28 0400Z',
   },
 ];
 
